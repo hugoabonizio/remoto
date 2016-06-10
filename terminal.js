0e22910da9e71a29e79ac1addaf1dda4dcabbfb3
hugo <hugo_abonizio@hotmail.com>
2016-06-10 12:33:22 -0300

    Handle errors and diconnections

--- terminal.js
+++ terminal.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+var WebSocket = require('ws')
+var pty = require('pty.js')
+
 function generate_token(size) {
   var text = ''
   var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -14,50 +17,65 @@ else
   host = 'localhost:8080'
 
 var token = generate_token(32)
+var ws
 
-var WebSocket = require('ws')
-var ws = new WebSocket('ws://' + host + '/?type=terminal&token=' + token)
-var pty = require('pty.js')
+function check() {
+  if (!ws || ws.readState == 3) connect()
+}
 
-var term = pty.fork('bash', [], {
-  name: require('fs').existsSync('/usr/share/terminfo/x/xterm-256color')
-    ? 'xterm-256color'
-    : 'xterm',
-  cols: 80,
-  rows: 24,
-  cwd: process.env.HOME
-})
+function connect() {
+  ws = new WebSocket('ws://' + host + '/?type=terminal&token=' + token)
 
-ws.on('open', function () {
-  console.log("Connection opened with token", token)
-
-  term.on('data', function (data) {
-    var message = JSON.stringify({ type: 'OUT', message: data })
-    ws.send(message)
+  var term = pty.fork('bash', [], {
+    name: require('fs').existsSync('/usr/share/terminfo/x/xterm-256color')
+      ? 'xterm-256color'
+      : 'xterm',
+    cols: 80,
+    rows: 24,
+    cwd: process.env.HOME
   })
 
-  setInterval(function () {
-    ws.send(JSON.stringify({ type: 'PING', message: 'PING!' }))
-  }, 1000)
-})
+  ws.on('open', function () {
+    console.log("Connection opened with token", token)
 
-ws.on('message', function (message) {
-  try {
-    var obj = JSON.parse(message)
-    if (obj.type == 'IN') {
-      term.write(obj.message)
+    term.on('data', function (data) {
+      var message = JSON.stringify({ type: 'OUT', message: data })
+      ws.send(message)
+    })
+
+    var ping = setInterval(function () {
+      try {
+        ws.send(JSON.stringify({ type: 'PING', message: 'PING!' }))
+      } catch (ex) {
+        clearInterval(ping)
+        // log exception
+      }
+    }, 1000)
+  })
+
+  ws.on('message', function (message) {
+    try {
+      var obj = JSON.parse(message)
+      if (obj.type == 'IN') {
+        term.write(obj.message)
+      }
+    } catch (ex) {
+      console.error(message, ex)
     }
-  } catch (ex) {
-    console.error(message, ex)
-  }
-})
+  })
 
-ws.on('error', function (e) {
-  console.log('Connection closed', e)
-  process.exit(1)
-})
+  ws.on('error', function (e) {
+    console.log('Connection closed', e)
+    ws = null
+    setTimeout(check, 1000)
+  })
 
-ws.on('close', function (e) {
-  console.log('Connection closed', e)
-  process.exit(1)
-})
+  ws.on('close', function (e) {
+    console.log('Connection closed', e)
+    ws = null
+    setTimeout(check, 1000)
+  })
+}
+
+connect()
+setInterval(check, 5000)
